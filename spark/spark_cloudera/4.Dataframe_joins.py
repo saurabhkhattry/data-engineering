28cc4d72d85dfaec9663ec2128dd49a166ef100a
Saurabh Khattry <khattry90@gmail.com>
2019-05-28 09:46:28 +0530

    Update 4.Dataframe_joins.py

--- spark/spark_cloudera/4.Dataframe_joins.py
+++ spark/spark_cloudera/4.Dataframe_joins.py
@@ -21,5 +21,25 @@ pcodeDF = spark.read\
     .option("Header", "True")\
     .csv("pcode.csv")
 
+print(" inner(default) ".center(50, '#'))
 peopleDF.join(pcodeDF, "pcode").show()
 
+"""
+Specify type of join as inner (default), outer, left_outer,
+right_outer, or leftsemi
+"""
+print(" left_outer ".center(50, '#'))
+peopleDF.join(pcodeDF, "pcode", "left_outer").show()
+print(" right_outer ".center(50, '#'))
+peopleDF.join(pcodeDF, "pcode", "right_outer").show()
+print(" leftsemi ".center(50, '#'))
+peopleDF.join(pcodeDF, "pcode", "leftsemi").show()
+print(" outer ".center(50, '#'))
+peopleDF.join(pcodeDF, "pcode", "outer").show()
+
+zcodeDF = spark.read\
+    .option("Header", "True")\
+    .csv("zcode.csv")
+
+print(" inner on col with different names ".center(50, '#'))
+peopleDF.join(zcodeDF, peopleDF.pcode == zcodeDF.zip).show()
